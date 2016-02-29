3a59cd6bf4cfd6e3e211436bec6d28a61e371a9d
Anton Bondarenko <bond.anton@gmail.com>
2016-02-29 10:10:55 +0300

    Critical changes needed to Cartesian module. Found bugs need to fix.

--- Space_visualization/space.py
+++ Space_visualization/space.py
@@ -5,15 +5,34 @@ from Space import Space
 
 class SpaceView(object):
 
-    def __init__(self, fig, space):
+    def __init__(self, fig, space, scale=1):
         self.fig = fig
+        if not isinstance(space, Space):
+            raise ValueError('Only Space class objects supported')
         self.space = space
+        self.scale = scale
+        self.cs_arrows = None
+        self.cs_labels = None
+        self.cs_cube = None
+
+    def draw_cs(self):
+        coordinate_system = self.space.basis_in_global_coordinate_system()
+        if self.cs_arrows is None:
+            self.cs_arrows, self.cs_labels = draw_CS_axes(self.fig, coordinate_system, offset=0, scale=self.scale)
+        else:
+            self.cs_arrows, self.cs_labels = update_CS_axes(coordinate_system, self.cs_arrows, self.cs_labels,
+                                                            offset=0, scale=self.scale)
 
 
-def draw_space(fig, space, scale=1):
+def gen_space_views(fig, space, scale=1):
     if not isinstance(space, Space):
         raise ValueError('argument has to be of Space class')
-    coordinate_system = space.basis_in_global_coordinate_system()
-    draw_CS_axes(fig, coordinate_system, scale=scale)
+    views = [SpaceView(fig, space, scale)]
     for key in space.elements.keys():
-        draw_space(fig, space.elements[key], scale=scale/2)
+        views += gen_space_views(fig, space.elements[key], scale=scale/2)
+    return views
+
+
+def draw_space(views):
+    for view in views:
+        view.draw_cs()
--- demo/05_space_visualization_planets.py
+++ demo/05_space_visualization_planets.py
@@ -10,35 +10,44 @@ solar_system = Space('Solar System')
 sun = Space('Sun')
 mercury = Space('Mercury', Cartesian(origin=[0.5, 0.5, 0.5]))
 venus = Space('Venus', Cartesian(origin=[1, 1, 1]))
-earth = Space('Earth', Cartesian(origin=[1.5, 1.5, 1.5]))
+#earth = Space('Earth', Cartesian(origin=[1.5, 1.5, 1.5]))
+earth = Space('Earth', Cartesian(origin=[0, 0, 0]))
 mars = Space('Mars', Cartesian(origin=[2, 2, 2]))
+#mars = Space('Mars', Cartesian(origin=[0, 0, 0]))
 
-solar_system.add_element(mercury)
-solar_system.add_element(venus)
+#solar_system.add_element(mercury)
+#solar_system.add_element(venus)
 solar_system.add_element(earth)
-solar_system.add_element(mars)
+#solar_system.add_element(mars)
 
 moon = Space('Moon', Cartesian(origin=[0.2, 0.2, 0.2]))
 earth.add_element(moon)
-lunohod = Space('Lunohod', Cartesian(origin=[0.01, 0.01, 0.01]))
+lunohod = Space('Lunohod', Cartesian(origin=[0.1, 0.1, 0.1]))
 moon.add_element(lunohod)
 
 phobos = Space('Phobos', Cartesian(origin=[0.2, 0.2, 0.2]))
-deimos = Space('Deimos', Cartesian(origin=[0.2, -0.2, 0.2]))
+deimos = Space('Deimos', Cartesian(origin=[-0.2, 0, 0]))
 mars.add_element(phobos)
 mars.add_element(deimos)
 
-earth.coordinate_system.rotate_axis_angle([1, 1, 1], np.deg2rad(45))
+#earth.coordinate_system.rotate_axis_angle([1, 1, 1], np.deg2rad(45))
+#mars.coordinate_system.rotate_axis_angle([1, 1, 1], np.deg2rad(45))
 
 fig = mlab.figure('CS demo', bgcolor=(0, 0, 0))  # Create the mayavi figure
 
-@mlab.animate(delay=10)
+@mlab.animate(delay=100)
 def anim():
-    Visual.draw_space(fig, solar_system)
+    views = Visual.gen_space_views(fig, solar_system)
+    #views = Visual.gen_space_views(fig, mars)
+    Visual.draw_space(views)
     while True:
-        #delta_eulers = np.array([direction_prec * np.deg2rad(step_prec), 0, direction_rot * np.deg2rad(step_rot)])
-        #CS_1.set_euler_angles(CS_1.euler_angles + delta_eulers)
-        #cs_box_1, arrows_1, labels_1 = Visual.update_CS_box(CS_1, cs_box_1, arrows_1, labels_1)
+        Visual.draw_space(views)
+        #coordinate_system = phobos.basis_in_global_coordinate_system()
+        #print coordinate_system.origin
+        earth.coordinate_system.rotate_axis_angle([1, 0, 1], np.deg2rad(1))
+        #mars.coordinate_system.rotate_axis_angle([-1, 1, 1], np.deg2rad(-1))
+        #mars.coordinate_system.rotate_axis_angle([1, 0, 0], np.deg2rad(1))
+        #print deimos.coordinate_system.origin
         yield
 
 anim()
