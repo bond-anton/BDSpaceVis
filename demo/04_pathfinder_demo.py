7149e836c115f2077c7da5968e2e871591e808b8
Anton Bondarenko <bond.anton@gmail.com>
2016-03-04 09:16:09 +0300

    Pathfinder refactoring sync.

--- demo/04_pathfinder_demo.py
+++ demo/04_pathfinder_demo.py
@@ -14,14 +14,14 @@ fig = mlab.figure('CS demo', bgcolor=(0, 0, 0))
 Visual.draw_coordinate_system_axes(fig, coordinate_system)
 
 right_helix = Helix(name='Right Helix', coordinate_system=coordinate_system,
-                    r=2, h=0.5, start=0, stop=np.pi * 2, right=True)
+                    radius=2, pitch=0.5, start=0, stop=np.pi * 4, right=True)
 left_helix = Helix(name='Left Helix', coordinate_system=coordinate_system,
-                   r=2, h=0.5, start=0, stop=np.pi * 2, right=False)
+                   radius=2, pitch=0.5, start=0, stop=np.pi * 2, right=False)
 
 print right_helix, left_helix
 print 'Helix length:', left_helix.length()
 
-t = np.linspace(0, np.pi*2, num=101, endpoint=True)
+t = np.linspace(0, np.pi*4, num=101, endpoint=True)
 local_points = right_helix.generate_points(t)
 global_points = right_helix.coordinate_system.to_parent(local_points)
 mlab.plot3d(global_points[:, 0], global_points[:, 1], global_points[:, 2], color=(1, 0, 0))
@@ -35,12 +35,13 @@ point1 = np.array([1, 1, 0])
 point2 = np.array([2, 2, 0])
 points = np.vstack((coordinate_system.to_parent(point1), coordinate_system.to_parent(point2)))
 mlab.points3d(points[:, 0], points[:, 1], points[:, 2], scale_factor=0.1)
-path = helix_between_two_points(coordinate_system, point1, point2, r=1, loops=7, right=True)
+path = helix_between_two_points(coordinate_system, point1, point2, radius=1, loops=7, right=True)
 t = np.linspace(path.start, path.stop, num=101 * (path.stop - path.start) / (2 * np.pi), endpoint=True)
 points = path.generate_points(t)
 global_points = path.coordinate_system.to_parent(points)
 mlab.plot3d(global_points[:, 0], global_points[:, 1], global_points[:, 2], color=(1, 0, 0))
 Visual.draw_coordinate_system_axes(fig, path.coordinate_system)
+
 
 '''
 path = arc_between_two_points(coordinate_system, point1, point2, r=1, right=True)
