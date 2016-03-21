2f32f7f724ac24b35e4ded8918d62dc9ce0e0279
Anton Bondarenko <bond.anton@gmail.com>
2016-03-22 00:56:19 +0300

    syncing work.

--- demo/09_scalar_field.py
+++ demo/09_scalar_field.py
@@ -13,7 +13,8 @@ class ChargedBall(Field):
     def scalar_field(self, xyz):
         print len(xyz.shape)
         x, y, z = xyz[0], xyz[1], xyz[2]
-        return np.sin(x*y*z)/(x*y*z)
+        r2 = x**2 + y**2 + z**2
+        return 1e3 / r2
 
     def vector_field(self, xyz):
         x, y, z = xyz[0], xyz[1], xyz[2]
@@ -27,10 +28,11 @@ charged_ball.add_element(electrostatic_field)
 fig = mlab.figure('CS demo', bgcolor=(0.0, 0.0, 0.0))  # Create the mayavi figure
 
 charged_ball_vis = Visual.FigureView(fig, charged_ball)
-charged_ball_vis.draw()
+#charged_ball_vis.draw()
 ball_field_vis = Visual.FieldView(fig, electrostatic_field)
 grid = np.mgrid[-10:10:100j, -20:20:200j, -10:10:100j]
 ball_field_vis.set_grid(grid)
+ball_field_vis.set_cs_visible(False)
 ball_field_vis.draw()
 
 mlab.show()
