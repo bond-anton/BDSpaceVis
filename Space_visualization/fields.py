ca16e5cd1f9871a7b2f78df5e3bfd4604bce4565
Anton Bondarenko <bond.anton@gmail.com>
2016-03-29 01:47:31 +0300

    Scalar and vector fields are both supported.

--- Space_visualization/fields.py
+++ Space_visualization/fields.py
@@ -15,8 +15,10 @@ class FieldView(SpaceView):
         self.grid = None
         self.scalar_data = None
         self.scalar_field = None
-        self.vector_data = None
         self.scalar_volume = None
+        self.vector_data = None
+        self.vector_field = None
+        self.vector_volume = None
         self.scalar_field_visible = scalar_field_visible
         self.vector_field_visible = vector_field_visible
         super(FieldView, self).__init__(fig, field, scale=scale, color=color, opacity=opacity,
@@ -32,34 +34,56 @@ class FieldView(SpaceView):
         self.update_scalar_data()
         self.update_vector_data()
 
+    def set_scalar_field_visible(self, scalar_field_visible=True):
+        self.scalar_field_visible = scalar_field_visible
+        self.draw()
+
+    def set_vector_field_visible(self, vector_field_visible=True):
+        self.vector_field_visible = vector_field_visible
+        self.draw()
+
     def update_scalar_data(self):
         self.scalar_data = self.space.scalar_field(self.grid)
 
     def update_vector_data(self):
         self.vector_data = self.space.vector_field(self.grid)
 
-    def draw_scalar_field(self):
-        if self.scalar_field_visible:
+    def draw_volume(self):
+        if self.vector_field_visible or self.scalar_field_visible:
             coordinate_system = self.space.basis_in_global_coordinate_system()
             grid = coordinate_system.to_parent(self.grid.reshape(3, -1).T).T.reshape(self.grid.shape)
-            if self.scalar_field is None:
+            if self.vector_field is None:
                 mlab.figure(self.fig, bgcolor=self.fig.scene.background)
-                self.scalar_field = mlab.pipeline.scalar_field(grid[0], grid[1], grid[2],
-                                                               self.scalar_data)
+                self.vector_field = mlab.pipeline.vector_field(grid[0], grid[1], grid[2],
+                                                               self.vector_data[0],
+                                                               self.vector_data[1],
+                                                               self.vector_data[2],
+                                                               scalars=self.scalar_data,
+                                                               name=self.space.name)
             else:
                 origin = np.array([np.min(grid[0]), np.min(grid[1]), np.min(grid[2])])
                 spacing = np.array([grid[0, 1, 0, 0] - grid[0, 0, 0, 0],
                                     grid[1, 0, 1, 0] - grid[1, 0, 0, 0],
                                     grid[2, 0, 0, 1] - grid[2, 0, 0, 0]])
-                self.scalar_field.spacing = spacing
-                self.scalar_field.origin = origin
-                self.scalar_field.scalar_data = self.scalar_data
-            if self.scalar_volume is None:
-                self.scalar_volume = mlab.pipeline.volume(self.scalar_field)
-        else:
-            if self.scalar_volume is not None:
-                self.scalar_volume.remove()
-            self.scalar_volume = None
+                self.vector_field.spacing = spacing
+                self.vector_field.origin = origin
+                self.vector_field.vector_data = np.rollaxis(self.vector_data, 0, 4)
+                self.vector_field.scalar_data = self.scalar_data
+            if self.vector_volume is None and self.vector_field_visible:
+                self.vector_volume = mlab.pipeline.vectors(self.vector_field, mask_points=20)
+            if self.scalar_volume is None and self.scalar_field_visible:
+                self.scalar_volume = mlab.pipeline.volume(self.vector_field)
+        if not self.vector_field_visible:
+            self.remove_vector_field()
+        if not self.scalar_field_visible:
+            self.remove_scalar_field()
 
-    def draw_volume(self):
-        self.draw_scalar_field()
+    def remove_vector_field(self):
+        if self.vector_volume is not None:
+            self.vector_volume.remove()
+        self.vector_volume = None
+
+    def remove_scalar_field(self):
+        if self.scalar_volume is not None:
+            self.scalar_volume.remove()
+        self.scalar_volume = None
--- demo/09_scalar_field.py
+++ demo/09_scalar_field.py
@@ -11,14 +11,13 @@ import Space_visualization as Visual
 class ChargedBall(Field):
 
     def scalar_field(self, xyz):
-        #print len(xyz.shape)
         x, y, z = xyz[0], xyz[1], xyz[2]
         r2 = x**2 + y**2 + z**2
         return 1e3 / r2
 
     def vector_field(self, xyz):
         x, y, z = xyz[0], xyz[1], xyz[2]
-        return x+y+z
+        return np.array([x, y, z])
 
 charged_ball = Sphere(name='Charged Ball', r_outer=0.5)
 electrostatic_field = ChargedBall(name='ball field', field_type='electrostatic')
@@ -30,7 +29,7 @@ fig = mlab.figure('CS demo', bgcolor=(0.0, 0.0, 0.0))  # Create the mayavi figur
 charged_ball_vis = Visual.FigureView(fig, charged_ball)
 #charged_ball_vis.draw()
 ball_field_vis = Visual.FieldView(fig, electrostatic_field)
-grid = np.mgrid[-10:10:100j, -20:20:200j, -10:10:100j]
+grid = np.mgrid[-10:10:100j, -20:20:200j, -10:10:50j]
 ball_field_vis.set_grid(grid)
 ball_field_vis.set_cs_visible(False)
 ball_field_vis.draw()
