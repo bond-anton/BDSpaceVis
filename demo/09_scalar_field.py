74ab9ff6d022955fe2b59fa633a489b2eed7a21b
Anton Bondarenko <bond.anton@gmail.com>
2016-03-29 20:59:40 +0300

    Superposition vector field example.

--- demo/09_scalar_field.py
+++ demo/09_scalar_field.py
@@ -25,17 +25,28 @@ class ChargedBall(Field):
         rtp[np.where(rtp[:, 0] < self.r), 0] = self.r
         r = rtp[:, 0] ** 2
         r = np.array([r, r, r]).T
-        return -self.q * xyz / r
+        return self.q * xyz / r
 
 pos_charged_ball = Sphere(name='Pos Charged Ball', r_outer=1.0)
 pos_charged_ball.coordinate_system.origin += np.array([-5, 0, 0])
 pos_electrostatic_field = ChargedBall(q=1, r=1, name='Pos Charged ball field', field_type='electrostatic')
 pos_charged_ball.add_element(pos_electrostatic_field)
 
+pos_charged_ball_2 = Sphere(name='Pos Charged Ball', r_outer=1.0)
+pos_charged_ball_2.coordinate_system.origin += np.array([0, -5, 0])
+pos_electrostatic_field_2 = ChargedBall(q=1, r=1, name='Pos Charged ball field', field_type='electrostatic')
+pos_charged_ball_2.add_element(pos_electrostatic_field_2)
+
+
 neg_charged_ball = Sphere(name='Neg Charged Ball', r_outer=1.0)
 neg_charged_ball.coordinate_system.origin += np.array([5, 0, 0])
 neg_electrostatic_field = ChargedBall(q=-1, r=1, name='Neg Charged ball field', field_type='electrostatic')
 neg_charged_ball.add_element(neg_electrostatic_field)
+
+neg_charged_ball_2 = Sphere(name='Neg Charged Ball', r_outer=1.0)
+neg_charged_ball_2.coordinate_system.origin += np.array([5, 0, 0])
+neg_electrostatic_field_2 = ChargedBall(q=-1, r=1, name='Neg Charged ball field', field_type='electrostatic')
+neg_charged_ball_2.add_element(neg_electrostatic_field_2)
 
 
 
@@ -44,15 +55,21 @@ fig = mlab.figure('CS demo', bgcolor=(0.0, 0.0, 0.0))  # Create the mayavi figur
 
 pos_charged_ball_vis = Visual.FigureView(fig, pos_charged_ball)
 pos_charged_ball_vis.set_color((1, 0, 0))
+pos_charged_ball_vis_2 = Visual.FigureView(fig, pos_charged_ball_2)
+pos_charged_ball_vis_2.set_color((1, 0, 0))
 neg_charged_ball_vis = Visual.FigureView(fig, neg_charged_ball)
-pos_charged_ball_vis.set_color((0, 0, 1))
+neg_charged_ball_vis.set_color((0, 0, 1))
+neg_charged_ball_vis_2 = Visual.FigureView(fig, neg_charged_ball_2)
+neg_charged_ball_vis_2.set_color((0, 0, 1))
 pos_charged_ball_vis.draw()
+pos_charged_ball_vis_2.draw()
 neg_charged_ball_vis.draw()
+neg_charged_ball_vis_2.draw()
 
 #pos_ball_field_vis = Visual.FieldView(fig, pos_electrostatic_field)
 #neg_ball_field_vis = Visual.FieldView(fig, neg_electrostatic_field)
 
-grid = np.mgrid[-10:10:10j, -5:5:10j, -5:5:10j]
+grid = np.mgrid[-10:10:20j, -10:10:20j, -5:5:10j]
 
 #pos_ball_field_vis.set_grid(grid)
 #pos_ball_field_vis.set_cs_visible(False)
@@ -60,7 +77,10 @@ grid = np.mgrid[-10:10:10j, -5:5:10j, -5:5:10j]
 #neg_ball_field_vis.set_grid(grid)
 #neg_ball_field_vis.set_cs_visible(False)
 
-superposed_field = SuperposedField('Superposed Field', [pos_electrostatic_field, neg_electrostatic_field])
+superposed_field = SuperposedField('Superposed Field', [pos_electrostatic_field,
+                                                        pos_electrostatic_field_2,
+                                                        neg_electrostatic_field,
+                                                        neg_electrostatic_field_2])
 #print superposed_field.scalar_field(np.array([[1,1,1], [2,2,2]]))
 superposed_field_vis = Visual.FieldView(fig, superposed_field)
 superposed_field_vis.set_grid(grid)
